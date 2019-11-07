ba4298a5ceb308da10b6b9096e4dd6cbd75f0683
hilmynaufal <55622577+hilmynaufal@users.noreply.github.com>
2019-11-07 20:35:38 +0700

    Add files via upload

---
+++ src/pbo10118045latihan59detectiveconan/PBO10118045Latihan59DetectiveConan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10118045latihan59detectiveconan;
+
+
+
+/**
+ *
+ * @author
+ *  Nama              : M. Hilmy Naufal
+ *  Kelas             : IF2
+ *  NIM               : 10118046
+ *  Deskripsi Program : membuat program berdasarkan karakter dari anime Detective Conan
+ * 
+ */
+public class PBO10118045Latihan59DetectiveConan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       Isi isi = new Isi();
+    }
+    
+}
---
+++ src/pbo10118045latihan59detectiveconan/Persamaan.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10118045latihan59detectiveconan;
+
+/**
+ *
+ * @author 
+ */
+public interface Persamaan {
+    void persamaan(int pilihan);
+    void tampilPersamaan();
+    
+}
